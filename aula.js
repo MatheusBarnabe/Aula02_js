0b1f424469e330026df12efcc168ed81adc6c111
Matheus Barnabé <matheusbp88@gmail.com>
2022-03-04 18:21:58 -0300

    Aula 03 da apostila 02

--- aula.js
+++ aula.js
@@ -47,7 +47,7 @@
 // console.log(nome)
 // nome[0]="nome" //n pode
 
-const nomes = ["Ana Maria", "Antonio", "Rodrigo", "Alex", "Cristina"]
+// const nomes = ["Ana Maria", "Antonio", "Rodrigo", "Alex", "Cristina"]
 
 // const resultante =  nomes.filter((n) => {
 //     return n.startsWith("")
@@ -59,5 +59,148 @@ const nomes = ["Ana Maria", "Antonio", "Rodrigo", "Alex", "Cristina"]
 // const res = nomes.map((x) => x.length)
 // console.log(res)
 
-const todosComecamComA = nomes.every ((n) => n.startsWith("A"))
-console.log(todosComecamComA)
+// const todosComecamComA = nomes.every ((n) => n.startsWith("A"))
+// console.log(todosComecamComA)
+
+// const valores = [1, 2, 3, 4]
+// //reduce
+// const soma = valores.reduce((ac, v) => ac + v)
+
+// function hello () {
+//     console.log ("Oi")
+// }
+// hello()
+// function hello (nome) {
+//     console.log ("Hello, " + nome)
+// }
+// hello ("João")
+
+// function soma (a, b) {
+//     return a + b
+// }
+// const res = soma (2, 3)
+// console.log(res)
+
+// const dobro = function (n){
+//     return 2 * n
+// }
+// console.log (dobro (5))
+
+// const triplo = function (n = 1){
+//     return n * 3
+// }
+// console.log(triplo())
+// console.log(triplo(2))
+
+//arrow functions
+// () => {}
+// const hello = () => console.log("Hello")
+// console.log(hello())
+
+// const dobro = (n) => {
+//     return n * 2
+// }
+// const dobro = (n) => n * 2
+
+// const dobro = n => n * 2
+// console.log (dobro(5))
+
+// const triplo = (n) => { n * 3 }
+// console.log(triplo(2))
+// Undefined por causa das chaves
+
+// const triplo = (n) => {
+//     const r = n + n
+//     r = r + n
+//     return (
+//         r + 2 + r + 2 + r + 2
+//     )
+// }
+// console.log(triplo(2))
+
+// Javascript Object Notification
+// JSON
+
+//pares chave/valor
+// let pessoa = {
+//     nome: "João",
+//     idade: 17
+// }
+
+// console.log (pessoa.nome)
+// console.log (pessoa['idade'])
+
+// let p = {
+//     nome: "Maria",
+//     idade: 22,
+//     endereco: {
+//         logradouro: "Rua B",
+//         numero: 12
+//     }
+// }
+
+// console.log (p.nome)
+// console.log (p['idade'])
+// console.log (p.endereco.logradouro)
+// console.log (p['endereco'].numero)
+// console.log (p['endereco']['numero'])
+
+//uma concessionaria que tem 
+//cnpj e endereco
+//e carros em estoque
+//cada carro tem, marca, modelo e ano de fab
+
+// let c = {
+//     cnpj: '11122211100001-45',
+//     edereco: {
+//         logadour: 'Rua A',
+//         numero: 10,
+//         bairro: "Vila J"
+//     },
+//     Veiculos: [
+//         {
+//             marca: 'Ford',
+//             modelo: 'Ka',
+//             ano: 2012,
+//             revisoes: [
+//                 {
+//                     data: '12/02/2013',
+//                     consultor: 'André'
+//                 },
+//                 {
+//                     data: '14/02/2014',
+//                     consultor: 'Pedro'
+//                 }
+//             ]
+//         }, 
+//         {
+//             marca: 'Volks',
+//             modelo: 'Nivus',
+//             ano: 2020
+//         }, 
+//         {
+//             marca: 'Chevrolet',
+//             model: 'Onix',
+//             ano: 2020
+//         }
+//     ]
+// }
+// console.log (c.Veiculos[0].revisoes[1].consultor) 
+
+// let calc = {
+//     soma: (a, b) => a + b,
+//     subtracao: function (a , b){
+//         return a - b
+//     }
+// }
+// console.log (calc.soma(2, 3))
+// console.log (calc.subtracao (5, 4))
+
+// const nome = "João"
+// const idade = 17
+// const objeto = { 
+//     nome, 
+//     idade 
+// }
+
+
