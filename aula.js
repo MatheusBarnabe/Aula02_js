f9991bc15299d5cb70c445096447fa60106481cc
Samuel <samuel_pds@hotmail.com>
2022-03-11 17:03:37 -0300

    Terminamos o bloco de codigos 3.3.3.. Retomar fazendo requisitos junto ao servico de previsao de tempo

--- aula.js
+++ aula.js
@@ -1,3 +1,95 @@
+function calculaRapidinho (n){
+    return Promise.resolve((n + (n + 1)) / 2)
+}
+
+function f (n){
+    return new Promise ((resolve, reject) => {
+        res = (n + (n + 1) / 2)
+        resolve(res)
+    })
+}
+
+f(10).then (res => console.log (res))
+
+
+// //1 + 2 ... + n-1 + n
+// function calculoDemorado (n){
+//     let p = new Promise ((resolve, reject) => {
+//         if (n < 0){
+//             reject('Somente valores positivos')
+//         }
+//         else{
+//             let res = 0
+//             for (let i = 1; i <= n; i++)
+//                 res += i
+//             resolve(res)
+//         }
+//     })
+//     return p
+// }
+
+// calculoDemorado(-10).then ((res) => {
+//     console.log ("Ok, deu certo: " + res)
+// }).catch (erro => {
+//     console.log ("Falou: " + erro)
+// })
+
+// const resultadoFuturo = calculoDemorado(10)
+// resultadoFuturo.then ((res) => console.log(res))
+
+
+// function exemplo (){
+//     let p = new Promise(function(resolve, reject){
+//         resolve(1)
+//     })
+//     return p
+// }
+
+// const resultadoFuturo = exemplo()
+// resultadoFuturo.then((resultado) => console.log(resultado))
+
+
+// function exemplo (){
+//     return 1
+// }
+
+// const resultado = exemplo()
+// console.log(resultado)
+
+
+// const fs = require ('fs') 
+// const abrirArquivo = function (nomeArquivo){ 
+//     const exibirConteudo = function (erro, conteudo){
+//         if (erro){
+//             console.log("Deu erro " + erro)
+//         }
+//         else{
+//             console.log (conteudo.toString())
+//             //processamento
+//             const dobro = 2 * +conteudo.toString()
+//             const finalizar = function(erro){
+//                 if (erro){
+//                     console.log("Deu erro: " + erro)
+//                 }
+//                 else{
+//                     console.log ("Salvou o dobro com sucesso")
+//                 }
+//             }
+//             fs.writeFile('dobro.txt', dobro.toString(), finalizar)
+//         }
+//     }
+//     //segundo parametro: função callback
+//     fs.readFile(nomeArquivo, exibirConteudo) 
+// } 
+
+// abrirArquivo('arquivo.txt')
+
+
+
+
+
+
+
 // const nome ="jose"
 // //nao pode atribuir outro valor a variavel
 
@@ -203,4 +295,7 @@
 //     idade 
 // }
 
+// const valores = [ 1, 2, 3, 4]
+// const soma = valores.reduce((ac, v)=> ac + v)
+// console.log(soma)
 
